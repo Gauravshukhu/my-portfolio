0757df7ecdcc82ce168f27b746de36557fd03826
Gaurav Shukla <gaurav639147@gmail.com>
2025-10-29 12:55:16 +0530

    Updated ThemeToggle to default dark mode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true); // default true
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
+
+    if (storedTheme === "light") {
       setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    } else {
+      // Default: dark mode ON
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   }, []);
 
@@ -32,8 +35,8 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-5 right-10 z-50 p-2 rounded-full transition-colors duration-300 ",
-        "focus:outlin-hidden"
+        "fixed max-sm:hidden top-5 right-10 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-none"
       )}
     >
       {isDarkMode ? (
